0480325f18215b554729856899a4cf1f73201f38
Игорь Павленко <illusiopard@gmail.com>
2023-03-19 16:27:51 +0300

    Добавьте файлы проекта.

---
+++ HW14.3.3/Program.cs
@@ -0,0 +1,30 @@
+﻿using HW14._3._3;
+
+namespace HW14._3._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //  создаём пустой список с типом данных Contact
+            var phoneBook = new List<Contact>();
+
+            // добавляем контакты
+            phoneBook.Add(new Contact("Игорь", "Николаев", 79990000001, "igor@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Довлатов", 79990000010, "serge@example.com"));
+            phoneBook.Add(new Contact("Анатолий", "Карпов", 79990000011, "anatoly@example.com"));
+            phoneBook.Add(new Contact("Валерий", "Леонтьев", 79990000012, "valera@example.com"));
+            phoneBook.Add(new Contact("Сергей", "Брин", 799900000013, "serg@example.com"));
+            phoneBook.Add(new Contact("Иннокентий", "Смоктуновский", 799900000013, "innokentii@example.com"));
+
+            var sortContacts = phoneBook.OrderBy(s => s.Name).ThenBy(s => s.LastName);
+
+            foreach (var item in sortContacts)
+            {
+                Console.WriteLine(item.Name + ", " + item.LastName);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
